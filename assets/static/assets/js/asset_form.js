cd1534ef10e4561c6620076cf49476becfb72ed0
PG Mohd Azhan Fikri <90368539+HarryDotMYx@users.noreply.github.com>
2025-01-13 11:53:28 +0800

    update baru
    Update baru

--- assets/static/assets/js/asset_form.js
+++ assets/static/assets/js/asset_form.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
             ['MSI', 'MSI'],
             ['Razer', 'Razer'],
             ['Samsung', 'Samsung']
+            ['GG', 'GG']
         ],
         server: [
             ['Dell', 'Dell (PowerEdge)'],
